fa55f93427244d530d87f5e9cb786d6bd120d15c
Denis Maslennikov <ktotom7@yandex.ru>
2023-10-24 18:36:46 +0300

    remove debug toolbar from middleware in prod

--- play_with_image/play_with_image/settings.py
+++ play_with_image/play_with_image/settings.py
@@ -86,8 +86,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+if DEBUG:
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
